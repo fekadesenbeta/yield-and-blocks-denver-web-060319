bca339e99f5250a4c8d3ffc3478754a865b1fb58
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-15 20:03:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(array)
   i = ["Tim", "Tom", "Jim"] 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+  do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-end
+
 #  your method here!
 
